9003dbd2e6ecbb8d398c371cb3efe3cabda176fd
Dan Engelbrecht <dan@engelbrecht.com>
2019-01-26 16:08:08 +0100

    Added optional sync-primitive

--- src/bikeshed.h
+++ src/bikeshed.h
@@ -6,12 +6,17 @@ namespace bikeshed
 {
 
 typedef struct Shed* HShed;
+struct SyncPrimitive
+{
+    bool (*AcquireLock)(SyncPrimitive* primitive);
+    void (*ReleaseLock)(SyncPrimitive* primitive);
+};
 
 typedef uint16_t TTaskID;
 typedef void (*TaskFunc)(HShed shed, TTaskID task_id, void* context_data);
 
 uint32_t GetShedSize(uint16_t max_task_count);
-HShed CreateShed(void* mem, uint16_t max_task_count);
+HShed CreateShed(void* mem, uint16_t max_task_count, SyncPrimitive* sync_primitive);
 
 bool CreateTasks(HShed shed, uint16_t task_count, TaskFunc* task_functions, void** task_context_data, TTaskID* out_task_ids);
 void FreeTasks(HShed shed, uint16_t task_count, const TTaskID* task_ids);
