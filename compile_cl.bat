e71d86d97b7c926d4f18a6a6d366fa1f487f47bb
Dan Engelbrecht <dan@engelbrecht.com>
2019-01-26 15:11:05 +0100

    Added containers dependency to get to jctest

--- compile_cl.bat
+++ compile_cl.bat
@@ -10,7 +10,7 @@ IF NOT EXIST build (
 
 pushd build
 
-cl.exe /nologo /Zi /O2 /D_CRT_SECURE_NO_WARNINGS /D_HAS_EXCEPTIONS=0 /EHsc /W4 ..\src\sogo.cpp ..\src\sogo_utils.cpp ..\src\sogo_nodes.cpp ..\third-party\xxHash-1\xxhash.c ..\test/main.cpp /link  /out:test.exe /pdb:test.pdb
+cl.exe /nologo /Zi /O2 /D_CRT_SECURE_NO_WARNINGS /D_HAS_EXCEPTIONS=0 /EHsc /W4 ..\src\bikeshed.cpp ..\test/main.cpp /link  /out:test.exe /pdb:test.pdb
 
 popd
 
