0f9ffa279c2bf21714b1269c0730db10b4c6f39c
Dan Engelbrecht <dan@engelbrecht.com>
2019-02-06 00:30:16 +0100

    Enabled asserts in debug clang build

--- compile_clang_debug.sh
+++ compile_clang_debug.sh
@@ -10,4 +10,4 @@ ASAN="-fsanitize=address -fno-omit-frame-pointer"
 CXXFLAGS="$CXXFLAGS -Wall -Weverything -pedantic -Wno-old-style-cast -Wno-sign-conversion -Wno-padded -Wno-unused-macros -Wno-c++98-compat -Wno-implicit-fallthrough -Wno-zero-as-null-pointer-constant"
 ARCH=-m64
 
-clang++ -o ./build/test_debug $OPT $DISASSEMBLY $ARCH -std=c++14 $CXXFLAGS $ASAN -Isrc src/bikeshed.cpp third-party/nadir/src/nadir.cpp test/main.cpp -pthread
+clang++ -o ./build/test_debug -DBIKESHED_ASSERTS $OPT $DISASSEMBLY $ARCH -std=c++14 $CXXFLAGS $ASAN -Isrc src/bikeshed.cpp third-party/nadir/src/nadir.cpp test/main.cpp -pthread
